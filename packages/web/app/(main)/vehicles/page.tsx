e46a68ecbdfc64afbf51d3efd549445666e4b990
suaebahmed <suaebahmed12@gmail.com>
2024-03-30 00:31:05 +0600

    added dynamic table data, recfor and added pagination button

--- packages/web/app/(main)/vehicles/page.tsx
+++ packages/web/app/(main)/vehicles/page.tsx
@@ -1,8 +1,66 @@
-import { getServerAuthSession } from "@/utils/auth";
-import VehiclesPage from "./vehicle";
+import Button from "@/components/button";
+import { Icon } from "@/components/icon";
+import { Badge } from "@/components/chip";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/table";
+import { Toggle } from "@/components/toggle";
+import { getAllVehicles } from "./server";
 
-export default async function IndexPage () {
-  const session = await getServerAuthSession();
-  return <VehiclesPage currentUserId={session.id} />;
-};
+const tableHeaders = [
+  "Vehicle Number",
+  "Vehicle Type",
+  "Capacity",
+  "Fuel Cost - Fully Loaded",
+  "Fuel Cost - Unloaded",
+];
 
+export default async function IndexPage() {
+  const vehicles = await getAllVehicles();
+
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-5">
+        <p className="text-lg font-bold">Manage Vehicles</p>
+      </div>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {tableHeaders.map((header, idx) => (
+              <TableHeader key={idx}>{header}</TableHeader>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {vehicles.map((vehicle, idx) => (
+            <TableRow key={idx}>
+              <TableCell>{vehicle.number}</TableCell>
+              <TableCell>{vehicle.type}</TableCell>
+              <TableCell>{vehicle.capacity}</TableCell>
+              <TableCell>{vehicle.loaded_cost} per km</TableCell>
+              <TableCell>{vehicle.unloaded_cost} per km</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className="flex justify-end my-4 gap-1">
+        <button>
+          <div className="bg-green-600 rounded-sm text-white">
+            <ChevronLeft />
+          </div>
+        </button>
+        <button>
+          <div className="bg-green-600 rounded-sm text-white">
+            <ChevronRight />
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+}
--- packages/web/app/(main)/vehicles/server.tsx
+++ packages/web/app/(main)/vehicles/server.tsx
@@ -1,24 +1,23 @@
 "use server";
 import { dbClient } from "@/client";
 
-export const getVehicles = async () => {
+export const getAllVehicles = async () => {
   return dbClient.vehicle.getAll();
 };
 export const getStss = async () => {
   return dbClient.sts.getAll();
 };
 
-export type VehiclesType = Awaited<ReturnType<typeof getVehicles>>;
+export type VehiclesType = Awaited<ReturnType<typeof getAllVehicles>>;
 
 export async function StsSelector() {
-  return <select
-    name="sts_id"
-  >
-    {
-      (await dbClient.sts.getAll()).map(sts => (
-        <option value={sts.id}>{sts.ward_number} ({sts.capacity_tonnes} Ton)</option>
-
-      ))
-    }
-  </select>
+  return (
+    <select name="sts_id">
+      {(await dbClient.sts.getAll()).map((sts, idx) => (
+        <option key={idx} value={sts.id}>
+          {sts.ward_number} ({sts.capacity_tonnes} Ton)
+        </option>
+      ))}
+    </select>
+  );
 }
