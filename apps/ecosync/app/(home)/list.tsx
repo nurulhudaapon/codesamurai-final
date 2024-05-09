e940a3191d0fb5872e1bf40809efd2591b143dc4
zahinafsar <afsarzahin@gmail.com>
2024-05-09 22:53:04 +0600

    feat: add mobile app

---
+++ apps/ecosync/app/(home)/list.tsx
@@ -0,0 +1,10 @@
+
+import { Text, View } from 'react-native';
+
+export default function ListScreen() {
+  return (
+    <View style={{flex: 1, padding: 20}}>
+      <Text>List page</Text>
+    </View>
+  );
+}
---
+++ apps/ecosync/app/_layout.tsx
@@ -0,0 +1,40 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+export { ErrorBoundary } from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  return (
+    <Stack>
+      <Stack.Screen name="(home)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
